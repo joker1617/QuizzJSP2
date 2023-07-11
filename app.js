f1c85cc428479d934965c15a4eea8da1cb38afa1
joker1617 <jeremybonnin@gmail.com>
2023-07-11 22:22:56 +0200

    ajout logo + début js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const responses = ["c", "a", "b", "a", "c"];
 const emojis = ["✔️", "✨", "👀", "😭", "👎"];
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", handleForm);
+
+function handleForm(e) {
+  e.preventDefault();
+
+  checkResponse();
+}
